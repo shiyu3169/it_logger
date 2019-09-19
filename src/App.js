ea64929cdbf670e67edf72dd1276915fecf4d27f
Shiyu Wang <shiyu3169@gmail.com>
2019-09-19 15:46:24 -0400

    tech list modal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
 import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
+import TechListModal from "./components/techs/TechListModal";
 
 function App() {
   useEffect(() => {
@@ -24,6 +25,7 @@ function App() {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </>
